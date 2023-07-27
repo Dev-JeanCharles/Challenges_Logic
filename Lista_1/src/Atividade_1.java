633ba40904760ae3556f7e16896f677d7c225920
Dev-JeanCharles <jeancduasan@gmail.com>
2023-07-27 09:23:56 -0300

    refactored atividade_1

---
+++ Lista_1/src/Atividade_1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+//Elabore um algoritmo que leia dois números e mostre a soma deles.
+public class Atividade_1 {
+    public void atividade(int var1, int var2){
+        System.out.println("Atividade1");
+        System.out.println("número 1: " +var1+ " - número 2: " +var2 );
+        System.out.println("A soma é igual: "+ (var1 + var2));
+    }
+}
--- Lista_1/src/Main.java
+++ Lista_1/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner sc = new Scanner(System.in);
+    private static Random rd = new Random();
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Atividade_1 atividade1 = new Atividade_1();
+        atividade1.atividade(rd.nextInt(0,1000),rd.nextInt(0,1000));
+
     }
 }
